8922f7db209d3c321a29141c7651b92309aad2c7
totoro <totoroyyw@gmail.com>
2021-11-12 14:32:47 +0800

    l3: format and init

--- server-main.c
+++ server-main.c
@@ -28,9 +28,9 @@
 #define BURST_SIZE 1
 
 static const struct rte_eth_conf port_conf_default = {
-	.rxmode = {
-		.max_rx_pkt_len = ETHER_MAX_LEN,
-	},
+    .rxmode = {
+        .max_rx_pkt_len = ETHER_MAX_LEN,
+    },
 };
 struct rte_mempool *mbuf_pool;
 
@@ -41,69 +41,71 @@ struct rte_mempool *mbuf_pool;
 static inline int
 port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 {
-	struct rte_eth_conf port_conf = port_conf_default;
-	const uint16_t rx_rings = 1, tx_rings = 1;
-	uint16_t nb_rxd = RX_RING_SIZE;
-	uint16_t nb_txd = TX_RING_SIZE;
-	int retval;
-	uint16_t q;
-	struct rte_eth_dev_info dev_info;
-	struct rte_eth_txconf txconf;
+    struct rte_eth_conf port_conf = port_conf_default;
+    const uint16_t rx_rings = 1, tx_rings = 1;
+    uint16_t nb_rxd = RX_RING_SIZE;
+    uint16_t nb_txd = TX_RING_SIZE;
+    int retval;
+    uint16_t q;
+    struct rte_eth_dev_info dev_info;
+    struct rte_eth_txconf txconf;
 
-	if (!rte_eth_dev_is_valid_port(port))
-		return -1;
+    if (!rte_eth_dev_is_valid_port(port))
+        return -1;
 
-	rte_eth_dev_info_get(port, &dev_info);
-	if (dev_info.tx_offload_capa & DEV_TX_OFFLOAD_MBUF_FAST_FREE)
-		port_conf.txmode.offloads |=
-			DEV_TX_OFFLOAD_MBUF_FAST_FREE;
+    rte_eth_dev_info_get(port, &dev_info);
+    if (dev_info.tx_offload_capa & DEV_TX_OFFLOAD_MBUF_FAST_FREE)
+        port_conf.txmode.offloads |=
+            DEV_TX_OFFLOAD_MBUF_FAST_FREE;
 
-	/* Configure the Ethernet device. */
-	retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
-	if (retval != 0)
-		return retval;
+    /* Configure the Ethernet device. */
+    retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
+    if (retval != 0)
+        return retval;
 
-	retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
-	if (retval != 0)
-		return retval;
+    retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
+    if (retval != 0)
+        return retval;
 
-	/* Allocate and set up 1 RX queue per Ethernet port. */
-	for (q = 0; q < rx_rings; q++) {
-		retval = rte_eth_rx_queue_setup(port, q, nb_rxd,
-				rte_eth_dev_socket_id(port), NULL, mbuf_pool);
-		if (retval < 0)
-			return retval;
-	}
+    /* Allocate and set up 1 RX queue per Ethernet port. */
+    for (q = 0; q < rx_rings; q++)
+    {
+        retval = rte_eth_rx_queue_setup(port, q, nb_rxd,
+                                        rte_eth_dev_socket_id(port), NULL, mbuf_pool);
+        if (retval < 0)
+            return retval;
+    }
 
-	txconf = dev_info.default_txconf;
-	txconf.offloads = port_conf.txmode.offloads;
-	/* Allocate and set up 1 TX queue per Ethernet port. */
-	for (q = 0; q < tx_rings; q++) {
-		retval = rte_eth_tx_queue_setup(port, q, nb_txd,
-				rte_eth_dev_socket_id(port), &txconf);
-		if (retval < 0)
-			return retval;
-	}
+    txconf = dev_info.default_txconf;
+    txconf.offloads = port_conf.txmode.offloads;
+    /* Allocate and set up 1 TX queue per Ethernet port. */
+    for (q = 0; q < tx_rings; q++)
+    {
+        retval = rte_eth_tx_queue_setup(port, q, nb_txd,
+                                        rte_eth_dev_socket_id(port), &txconf);
+        if (retval < 0)
+            return retval;
+    }
 
-	/* Start the Ethernet port. */
-	retval = rte_eth_dev_start(port);
-	if (retval < 0)
-		return retval;
+    /* Start the Ethernet port. */
+    retval = rte_eth_dev_start(port);
+    if (retval < 0)
+        return retval;
 
-	/* Display the port MAC address. */
-	struct ether_addr addr;
-	rte_eth_macaddr_get(port, &addr);
-	printf("Port %u MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
-			   " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
-			port,
-			addr.addr_bytes[0], addr.addr_bytes[1],
-			addr.addr_bytes[2], addr.addr_bytes[3],
-			addr.addr_bytes[4], addr.addr_bytes[5]);
+    /* Display the port MAC address. */
+    struct ether_addr addr;
+    rte_eth_macaddr_get(port, &addr);
+    printf("Port %u MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
+           " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
+           port,
+           addr.addr_bytes[0], addr.addr_bytes[1],
+           addr.addr_bytes[2], addr.addr_bytes[3],
+           addr.addr_bytes[4], addr.addr_bytes[5]);
 
-	/* Enable RX in promiscuous mode for the Ethernet device. */
-	rte_eth_promiscuous_enable(port);
+    /* Enable RX in promiscuous mode for the Ethernet device. */
+    rte_eth_promiscuous_enable(port);
 
-	return 0;
+    return 0;
 }
 
 /*
@@ -111,11 +113,9 @@ port_init(uint16_t port, struct rte_mempool *mbuf_pool)
  * basing on information from the query packet(buf2).
  */
 static void
-build_packet(char *buf1, char * buf2, uint16_t pkt_size)
+build_packet(char *buf1, char *buf2, uint16_t pkt_size)
 {
-
 }
-
 
 /*
  * The lcore main. This is the main thread that does the work, read
@@ -124,110 +124,120 @@ build_packet(char *buf1, char * buf2, uint16_t pkt_size)
 static __attribute__((noreturn)) void
 lcore_main(void)
 {
-	uint16_t port = 0;	                // only one port is used.
-	// uint8_t query_buf_flag = 0;		// set to 1 after query packet received.
-	struct rte_mbuf *query_buf[BURST_SIZE], *reply_buf[BURST_SIZE];
-	uint16_t nb_rx, nb_tx;
-	uint8_t *buffer;
-	struct Message msg;
-	memset(&msg, 0, sizeof(struct Message));
-	memset(query_buf, 0, BURST_SIZE);
-	memset(reply_buf, 0, BURST_SIZE);
-	
-	/*
+    uint16_t port = 0; // only one port is used.
+    // uint8_t query_buf_flag = 0;		// set to 1 after query packet received.
+    struct rte_mbuf *query_buf[BURST_SIZE], *reply_buf[BURST_SIZE];
+    uint16_t nb_rx, nb_tx;
+    uint8_t *buffer;
+    struct Message msg;
+    memset(&msg, 0, sizeof(struct Message));
+    // memset(query_buf, 0, BURST_SIZE);
+    // memset(reply_buf, 0, BURST_SIZE);
+
+    /*
 	 * Check that the port is on the same NUMA node as the polling thread
 	 * for best performance.
 	 */
-	if (rte_eth_dev_socket_id(port) > 0 &&
-			rte_eth_dev_socket_id(port) !=
-					(int)rte_socket_id())
-		printf("WARNING, port %u is on remote NUMA node to "
-				"polling thread.\n\tPerformance will "
-				"not be optimal.\n", port);
-	
-	printf("\nSimpleDNS (using DPDK) is running...\n");
+    if (rte_eth_dev_socket_id(port) > 0 &&
+        rte_eth_dev_socket_id(port) !=
+            (int)rte_socket_id())
+        printf("WARNING, port %u is on remote NUMA node to "
+               "polling thread.\n\tPerformance will "
+               "not be optimal.\n",
+               port);
 
-	/* Run until the application is quit or killed. */
-	for (;;) {
-		// Add your code here.
-		// Part 0. 
-		
-		/*********preparation (begin)**********/
-		free_questions(msg.questions);
-		free_resource_records(msg.answers);
-		free_resource_records(msg.authorities);
-		free_resource_records(msg.additionals);
-		memset(&msg, 0, sizeof(struct Message));
-		/*********preparation (end)**********/
-		
-		// Add your code here.
-		// Part 1.
-			
-		/*********read input (begin)**********/ 
-		// not DNS
-		if (decode_msg(&msg, buffer, query_buf[i]->data_len - 42) != 0) {
-			continue;
-		}
-		/* Print query */
-		print_query(&msg);
+    printf("\nSimpleDNS (using DPDK) is running...\n");
 
-		resolver_process(&msg);
+    /* Run until the application is quit or killed. */
+    for (;;)
+    {
+        // Add your code here.
+        int i = 0;
+        // Part 0.
 
-		/* Print response */
-		print_query(&msg);
-		/*********read input (end)**********/
-		
-		//Add your code here.
-		//Part 2.
-		
-		/*********write output (begin)**********/
-		uint8_t *p = buffer;
-		if (encode_msg(&msg, &p) != 0) {
-			continue;
-		}
 
-		uint32_t buflen = p - buffer;
-		/*********write output (end)**********/
-		
-		//Add your code here.
-		//Part 3.
-		
-	}
+        /*********preparation (begin)**********/
+        free_questions(msg.questions);
+        free_resource_records(msg.answers);
+        free_resource_records(msg.authorities);
+        free_resource_records(msg.additionals);
+        memset(&msg, 0, sizeof(struct Message));
+        /*********preparation (end)**********/
+
+        // Add your code here.
+        // Part 1.
+
+        /*********read input (begin)**********/
+        // not DNS
+        if (decode_msg(&msg, buffer, query_buf[i]->data_len - 42) != 0)
+        {
+            //Add totoro code here.
+            // rte_pktmbuf_free(query_buf[i]);
+            continue;
+        }
+        /* Print query */
+        print_query(&msg);
+
+        resolver_process(&msg);
+
+        /* Print response */
+        print_query(&msg);
+        /*********read input (end)**********/
+
+        //Add your code here.
+        //Part 2.
+
+        /*********write output (begin)**********/
+        uint8_t *p = buffer;
+        if (encode_msg(&msg, &p) != 0)
+        {
+            //Add totoro code here.
+            // rte_pktmbuf_free(query_buf[i]);
+            continue;
+        }
+
+        uint32_t buflen = p - buffer;
+        /*********write output (end)**********/
+
+        //Add your code here.
+        rte_pktmbuf_append(reply_buf[i], buflen + 42 - reply_buf[i]->data_len); // 42 is the size of header.
+        // build_packet(reply_buf[i]->data, buffer, buflen);
+        //Part 3.
+    }
 }
 
 /*
  * The main function, which does initialization and calls the per-lcore
  * functions.
  */
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	uint16_t portid = 0, nb_ports = 1;
+    uint16_t portid = 0, nb_ports = 1;
 
-	/* Initialize the Environment Abstraction Layer (EAL). */
-	int ret = rte_eal_init(argc, argv);
-	if (ret < 0)
-		rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
+    /* Initialize the Environment Abstraction Layer (EAL). */
+    int ret = rte_eal_init(argc, argv);
+    if (ret < 0)
+        rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
 
-	argc -= ret;
-	argv += ret;
+    argc -= ret;
+    argv += ret;
 
-	/* Creates a new mempool in memory to hold the mbufs. */
-	mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
-		MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+    /* Creates a new mempool in memory to hold the mbufs. */
+    mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
+                                        MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
 
-	if (mbuf_pool == NULL)
-		rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
+    if (mbuf_pool == NULL)
+        rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
 
-	/* Initialize port 0. */
-	if (port_init(portid, mbuf_pool) != 0)
-		rte_exit(EXIT_FAILURE, "Cannot init port %"PRIu16 "\n", portid);
+    /* Initialize port 0. */
+    if (port_init(portid, mbuf_pool) != 0)
+        rte_exit(EXIT_FAILURE, "Cannot init port %" PRIu16 "\n", portid);
 
-	if (rte_lcore_count() > 1)
-		printf("\nWARNING: Too many lcores enabled. Only 1 used.\n");
+    if (rte_lcore_count() > 1)
+        printf("\nWARNING: Too many lcores enabled. Only 1 used.\n");
 
-	/* Call lcore_main on the master core only. */
-	lcore_main();
+    /* Call lcore_main on the master core only. */
+    lcore_main();
 
-	return 0;
+    return 0;
 }
